66bf0051ece2317c3d2b1f88781a8d1c925624b0
anukultyagi <anukultyagi321@gmail.com>
2024-10-28 22:36:57 +0530

    resolved build issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { QrContextProvider } from './contexts/Qrcontext.jsx'
+import { QrContextProvider } from './contexts/QrContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QrContextProvider>
